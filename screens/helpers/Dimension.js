24df2ccff100a9c3c02a89ef526872e91f1476c3
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-02-13 22:46:33 +0700

    feat!: old screens

--- screens/helpers/Dimension.js
+++
@@ -1,8 +0,0 @@
-// Import Core Libraries
-import { Dimensions } from 'react-native';
-
-// Get Width Value
-export const windowWidth = Dimensions.get('window').width;
-
-// Get Height Value
-export const windowHeight = Dimensions.get('window').height;
