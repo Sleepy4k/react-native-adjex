996e399bcc207292e941684173830bbd9aeae49c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-04-04 04:55:48 +0700

    feat: some constant

---
+++ src/constants/margin.js
@@ -0,0 +1,13 @@
+const margin = {
+  mg_rlg: -20,
+  mg_xs: 5,
+  mg_sm: 10,
+  mg_md: 15,
+  mg_lg: 20,
+  mg_xl: 25,
+  mg_2xl: 30,
+};
+
+export default margin;
+
+// Path: src\constants\margin.js
---
+++ src/constants/padding.js
@@ -0,0 +1,12 @@
+const padding = {
+  pd_xs: 5,
+  pd_sm: 10,
+  pd_md: 15,
+  pd_lg: 20,
+  pd_xl: 25,
+  pd_2xl: 30,
+};
+
+export default padding;
+
+// Path: src\constants\padding.js
