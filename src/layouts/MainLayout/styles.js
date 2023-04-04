1694ea56d7d7496ec6c29cf05436a01bb08fd9fa
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-04-05 04:42:16 +0700

    chore: main layout

---
+++ src/layouts/MainLayout/styles.js
@@ -0,0 +1,13 @@
+import { colors } from "@constants";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    backgroundColor: colors.lightDeepBlue,
+  },
+});
+
+export default styles;
+
+// Path: src\layouts\MainLayout\styles.js
