ed69f97a55da923b2c4614ed8a2cf203771340a0
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-04-10 22:01:17 +0700

    feat: some screen

---
+++ src/screens/AddQuiz/styles.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 35,
+    backgroundColor: "#1C3144",
+  },
+  card: {
+    marginTop: 10,
+    backgroundColor: "white",
+    height: 960,
+    width: 300,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+  logo: {
+    height: 90,
+    width: 270,
+    marginBottom: 10,
+    borderRadius: 5,
+    alignSelf: "center",
+  },
+  tombol: {
+    marginTop: 20,
+    backgroundColor: "white",
+    height: 60,
+    width: 300,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+  cardtext: {
+    marginTop: 10,
+    marginLeft: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 10,
+    fontSize: 16,
+    height: 100,
+    width: 260,
+    backgroundColor: "none",
+  },
+  error_teks: {
+    color: "red",
+    marginTop: 10,
+    alignSelf: "center",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+});
+
+export default styles;
+
+// Path: src\screens\AddQuiz\styles.js
